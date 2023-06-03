94ecceee8114a007c0bcdcc081cc6c517a138d2f
ActuallyTaylor <zachary.lineman@gmail.com>
2023-06-03 13:25:14 -0400

    Small change to the intent

--- Actions/GetShortcutHandler.swift
+++ Actions/GetShortcutHandler.swift
@@ -9,14 +9,7 @@ import Intents
 import HydrogenReporter
 
 class GetShortcutHandler: NSObject, GetShortcutIntentHandling {
-    func handle(intent: GetShortcutIntent) async -> GetShortcutIntentResponse {
-        guard let defaults = UserDefaults(suiteName: Constants.suiteName) else {
-            LOG("Unable to create shared User Defaults", level: .error)
-            let response = GetShortcutIntentResponse(code: .failure, userActivity: .none)
-            response.error = "No Defaults Available"
-            return response
-        }
-        
+    func handle(intent: GetShortcutIntent) async -> GetShortcutIntentResponse {        
         guard let name = SharedDataStorageManager.defaults.string(forKey: DefaultsKeys.lastSignedShortcutNameKey) else {
             return GetShortcutIntentResponse(code: .noName, userActivity: nil)
         }
